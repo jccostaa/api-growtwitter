3619770c25fbd2b1ca44d3a0e007b183c585c016
jccostaa <dev.jeancosta@gmail.com>
2024-05-12 16:04:24 -0300

    implement JTW token

--- src/dtos/auth.dto.ts
+++ src/dtos/auth.dto.ts
@@ -1,4 +1,10 @@
+import { JwtPayload } from "jsonwebtoken"
+
 export interface AuthDTO{
     email: string
     senha: string
+}
+
+export interface PayloadToken extends JwtPayload{
+    id:string
 }
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,7 +1,8 @@
 import { repository } from "../database/prisma.connection";
-import { randomUUID } from "crypto";
+//import { randomUUID } from "crypto";
 import { ResponseData } from "../dtos/response.dto";
-import { AuthDTO } from "../dtos/auth.dto";
+import { AuthDTO, PayloadToken } from "../dtos/auth.dto";
+import jwt from "jsonwebtoken"
 
 
 
@@ -18,35 +19,78 @@ export class AuthService {
         //se nao tiver usuario, 401 - unauthoridez
         if (!usuario) {
             return {
+                success: false,
+                message: "Credenciais invalidas",
+                code: 401
+            }
+        }
+
+        //const token = randomUUID();
+
+        const usuario_id = {
+            id: usuario.id
+        }
+        //const token = jwt.sign(user_id, process.env.JWT_SECRET!) exclamação para avisar que é garantido que virá uma informação
+
+        const token = this.generateToken(usuario_id)
+
+        // await repository.usuario.update({
+        //     where: {
+        //         id: usuario.id
+        //     },
+        //     data: {
+        //         token
+        //     }
+        // });
+
+        return {
+            success: true,
+            message: "Login efetuado com sucesso",
+            code: 200,
+            data: {
+                id: usuario.id,
+                email: usuario.email,
+                nome: usuario.nome,
+                nomeUsuario: usuario.nomeUsuario,
+                token
+            }
+        };
+    }
+
+    public async validateLogin(token: string, id: string): Promise<ResponseData> {
+
+        const payload = this.validateToken(token) as PayloadToken
+
+        if (payload === null || id !== payload.id) {
+            return {
                 success:false,
-                message:"Credenciais invalidas",
+                message:"Token inválido",
                 code:401
             }
         }
 
-        const token = randomUUID();
-        const userId = usuario.id;
-
-        await repository.usuario.update({
-            where: {
-                id: usuario.id
-            },
-            data: {
-                token
-            }
-        });
-
-        return {
+        return{
             success:true,
-            message:"Login efetuado com sucesso",
-            code:200,
-            data:{
-                id:usuario.id,
-                email:usuario.email,
-                nome:usuario.nome,
-                nomeUsuario:usuario.nomeUsuario,
-                token
-            }
-        };
+            message:"Validação executada com sucesso",
+            code:200
+        }
+    }
+
+    public generateToken(payload: any) {
+        const token = jwt.sign(payload, process.env.JWT_SECRET!, {
+            expiresIn: '1d'
+        })
+
+        return token
+    }
+
+    public validateToken(token: string) {
+        try {
+            const payload = jwt.verify(token, process.env.JWT_SECRET!)
+
+            return payload
+        } catch (error: any) {
+            return null
+        }
     }
 } 
